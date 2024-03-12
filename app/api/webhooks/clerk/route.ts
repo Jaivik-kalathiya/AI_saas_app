322377aea01d25abb1b368fcaa0d077d8df4f2f7
Jaivik Kalathiya <jaivikkalathiya@gmail.com>
2024-03-12 20:38:23 +0530

    Added webhook

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -56,10 +56,13 @@ export async function POST(req: Request) {
   // Get the ID and type
   const { id } = evt.data;
   const eventType = evt.type;
-
+//   console.log(evt);÷
   // CREATE
   if (eventType === "user.created") {
-    const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
+    const { id, email_addresses, image_url, first_name, last_name, username } =
+      evt.data;
+
+    console.log("user created",evt);
 
     const user = {
       clerkId: id,
--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,31 +1,53 @@
 
-import mongoose, { Mongoose } from "mongoose";
+// import mongoose, { Mongoose } from "mongoose";
 
-const MONGODB_URL = process.env.MONGODB_URL;
+// const MONGODB_URL = process.env.MONGODB_URL;
 
-interface MongooseConnection {
-  conn: Mongoose | null;
-  promise: Promise<Mongoose> | null;
-}
+// interface MongooseConnection {
+//   conn: Mongoose | null;
+//   promise: Promise<Mongoose> | null;
+// }
 
-let cached: MongooseConnection = (global as any).mongoose;
+// let cached: MongooseConnection = (global as any).mongoose;
 
-if (!cached) {
-  cached = (global as any).mongoose = { conn: null, promise: null };
-}
-export const connectToDatabase = async () => {
-  if (!MONGODB_URL) throw new Error("Missing MONGODB_URL");
+// if (!cached) {
+//   cached = (global as any).mongoose = { conn: null, promise: null };
+// }
+// export const connectToDatabase = async () => {
+//   if (!MONGODB_URL) throw new Error("Missing MONGODB_URL");
 
-  cached.promise =
-    cached.promise ||
-    mongoose.connect(MONGODB_URL, {
-      dbName: "imaginify",
-      bufferCommands: false,
-    });
+//   cached.promise =
+//     cached.promise ||
+//     mongoose.connect(MONGODB_URL, {
+//       dbName: "Imaginify",
+//       bufferCommands: false,
+//     });
     
-    cached.conn = await cached.promise;
+//     cached.conn = await cached.promise;
 
 
+// };
+
+import mongoose from "mongoose";
+
+let isConnected = false;
+
+export const connectToDatabase = async () => {
+  mongoose.set("strictQuery", true);
+
+  if (!process.env.MONGODB_URL)
+    return console.log("No MONGODB_URL found in .env file");
+
+  if (isConnected) return console.log("Already connected to DB");
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+
+    isConnected = true;
+    console.log("Connection to DB successful");
+  } catch (error) {
+    console.log("Error connecting to DB: ", error);
+  }
 };
 
 
